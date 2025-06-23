cefaa9244a7bf97b9a5874766406576114ee1a1d
Akshat-Srivastava2004 <akshatsri2004alld@gmail.com>
2025-06-23 09:04:31 +0530

    dynamically content coming in the home page

--- src/app/Pages/home/home.ts
+++ src/app/Pages/home/home.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Navbar } from "../navbar/navbar";
+import { CompetitionService } from '../../services/competition';
+import { CompetitionModel } from '../../model/competition.model';
+import { FormsModule } from '@angular/forms';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FormsModule,DatePipe],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class Home {
+export class Home implements OnInit{
+     getalldata=inject(CompetitionService)
+     gridlist:CompetitionModel[]=[];
+
+
+     ngOnInit(): void {
+      debugger
+         this.getAll()
+     }
+
+     getAll(){
+      debugger
+      this.getalldata.getCompetition().subscribe({
+        next:(result)=>{
+          debugger
+          this.gridlist=result;
+        },
+        error:(err:any)=>{
+          debugger
+          console.log("the error is ",err)
+          alert("Got an error ")
+        }
+      })
+     }
 
 }
